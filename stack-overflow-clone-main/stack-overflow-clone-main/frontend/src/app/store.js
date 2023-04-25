fa6b5742f02ca671efa8a0d013fe60b1fa994097
srushtinagrale <104786617+srushtinagrale@users.noreply.github.com>
2023-04-25 20:48:26 +0530

    Update store.js

--- stack-overflow-clone-main/stack-overflow-clone-main/frontend/src/app/store.js
+++ stack-overflow-clone-main/stack-overflow-clone-main/frontend/src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../feature/userSlice";
 // import threadReducer from "../features/counter/threadSlice";
-
 export default configureStore({
   reducer: {
     user: userReducer,
